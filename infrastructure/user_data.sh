fa7620b7c93cb454d69c8f4b47f5a03ad5252bd1
Jonathan Schnabel <jonathan.schnabel31@gmail.com>
2020-04-10 22:39:03 -0400

    debug user_data

--- infrastructure/user_data.sh
+++ infrastructure/user_data.sh
@@ -13,39 +13,32 @@ amazon-linux-extras install -y epel
 yum-config-manager --enable epel*
 yum install -y git python3 gcc postgresql-devel python3-devel.x86_64 tor
 
-cat << EOT >> /etc/tor/torrc
+pip3 install bs4 torrequest awscli boto3
+
+TOR_PASSWORD=ind*Ica69
+
+cat << EOT > /etc/tor/torrc
 SOCKSPort 9050
 ControlPort 9051
+CookieAuthentication 1
 EOT
 
-echo "export TOR_PASSWORD=ind*Ica69" >> /home/ec2-user/.bashrc
-echo "export AWS_REGION=us-east-1" >> /home/ec2-user/.bashrc
+echo "export TOR_PASSWORD=$TOR_PASSWORD" >> /home/ec2-user/.bashrc
 echo "HashedControlPassword $(tor --hash-password "$TOR_PASSWORD" | grep --color=never 16:[A-Z0-9])" >> /etc/tor/torrc
 systemctl restart tor
 
-pip3 install bs4 torrequest awscli boto3
+git clone https://github.com/jschnab/real-estate-scraping.git /home/ec2-user/real-estate-scraping
 
-mkdir /home/ec2-user/.browsing
-aws s3 sync s3://jschnab-test-bucket/browser_config/ /home/ec2-user/.browsing/
+mkdir /home/ec2-user/.aws
+cat << EOT > /home/ec2-user/.aws
+[default]
+region = us-east-1
+output = json
+EOT
 
-cat << EOF > /home/ec2-user/.browsing/browser.conf
-[logging]
-log_file = /home/ec2-user/.browsing/browser.log
+aws s3 cp --recursive s3://jschnab-test-bucket/rea-estate/browser_config /home/ec2-user/.browsing
 
-[harvest]
-archive_dir = /home/ec2-user/real-estate-scraping/nytimes/archives
-key_prefix = real-estate/nytimes/harvest
-
-[extract]
-csv_path = /home/ec2-user/real-estate-scraping/nytimes/rentals.csv
-csv_header = listing_type,property_type,burrough,neighborhood,address,zip,price,description,amenities,common_charges,monthly_taxes,days_listed,size,year_built,bedrooms,bathrooms,half_bathrooms,rooms,representative,agency
-
-[sqs]
-queue_url = https://sqs.us-east-1.amazonaws.com/941435572757/toparse.fifo
-
-[s3]
-bucket = jschnab-test-bucket
-EOF
+chown -R ec2-user:ec2-user /home/ec2-user/
 
 date "+%Y-%m-%d %H:%M:%S"
 ENDTIME=$(date +%s)
